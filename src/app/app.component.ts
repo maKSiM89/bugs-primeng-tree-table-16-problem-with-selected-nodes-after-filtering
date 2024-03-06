20181ea3e15173ca38a9a5793607306cbe489039
maKSiM89 <maksim4uk@gmail.com>
2024-03-06 14:44:37 +0200

    remove etxra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TreeTableNode } from 'primeng/api';
 import { FILES } from './const/files.const';
 import { TreeTable } from 'primeng/treetable';
@@ -7,50 +7,13 @@ import { TreeTable } from 'primeng/treetable';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   selectedNodes: TreeTableNode | TreeTableNode[] | null = [];
-  selectedAccountsIds = [1,2,3,4,5,6];
   filterValue: string = '';
   readonly files = FILES;
-
-  ngOnInit(): void {
-    this.selectedNodes = [];
-
-    this.selectedAccountsIds.forEach((id: number) => {
-      const key: string = id.toString();
-      const selectedNodeIndex = Array.isArray(this.selectedNodes) ? this.selectedNodes.findIndex(
-        (node: TreeTableNode) => node.key === key
-      ) : -1;
-
-      if (selectedNodeIndex === -1) {
-        const node = this.getNodeWithKey(key, this.files);
-
-        if (node) {
-          Array.isArray(this.selectedNodes) && this.selectedNodes.push(node);
-        }
-      }
-    });
-  }
 
   onFilter(table: TreeTable, name: string): void {
     table.filter(this.filterValue, name, 'contains');
   }
 
-  getNodeWithKey(key: string, nodes: TreeTableNode[]): TreeTableNode | undefined {
-    for (const node of nodes) {
-      if (node.key === key) {
-        return node;
-      }
-
-      if (node.children) {
-        const matchedNode = this.getNodeWithKey(key, node.children);
-
-        if (matchedNode) {
-          return matchedNode;
-        }
-      }
-    }
-
-    return undefined;
-  }
 }
